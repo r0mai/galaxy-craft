79ecb0e958d673e50bb5fb48a0e8db1bcf3b48d2
András Kucsma <r0mai@users.noreply.github.com>
2012-08-08 12:53:59 +0000

    added support for comments ('#') and other config file includes () for include files

--- src/configreader.cpp
+++ src/configreader.cpp
@@ -3,6 +3,8 @@
 
 #include <fstream>
 
+#include <boost/algorithm/string.hpp>
+
 namespace gc {
 
 configreader::configreader() {}
@@ -21,12 +23,28 @@ void configreader::read_config(const std::string& file) {
 
 	std::string line;
 	while( std::getline( in, line ) ) {
+
+		boost::trim( line );
+
+		if ( line.empty() || line[0] == '#' ) { //empty line, or comment
+			continue;
+		}
+
 		const unsigned separator = line.find( '=', 0 );
 		if ( separator != std::string::npos ) {
-			const std::string key = line.substr(0, separator);
-			const std::string value = line.substr(separator+1);
+			std::string key = line.substr(0, separator);
+			std::string value = line.substr(separator+1);
 
-			add_value(key, value);
+			boost::trim( key );
+			boost::trim( value );
+
+			if ( key == "$include" ) {
+				std::cout << "reading file=\""<< value << "\"\n";
+				read_config(value); //TODO infinite recursion possible : check if we've read this file already
+			} else {
+				std::cout << "key=\"" << key << "\", \"" << value << "\"\n";
+				add_value(key, value);
+			}
 		}
 	}
 }
